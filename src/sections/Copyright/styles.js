41afa77d2a7f6d6a8b35f14ed85ffae8563e3f48
danimnh <danimnh4@gmail.com>
2021-02-05 23:34:16 +0700

    adding logo, ipb arung kementan

--- src/sections/Copyright/styles.js
+++ src/sections/Copyright/styles.js
@@ -1,8 +1,34 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   copyright: {
-    'user-select': 'none',
+    "user-select": "none",
+    paddingTop: "10px",
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+
+    // width: "300px",
+    // "justify-content": "space-between",
+    // marginTop: "20px",
+    // height: `calc(100% - ${theme.mixins.toolbar.minHeight + 8}px)`,
+  },
+  contentHorizontal: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  logo: {
+    paddingTop: "4px",
+    paddingRight: "10px",
+    alignSelf: "center",
+    height: "42px",
+  },
+  logoBigger: {
+    alignSelf: "center",
+    height: "60px",
   },
 }));
 
