216bdc04177c3d20fa243cfa3c3db6dd541232a6
danimnh <danimnh4@gmail.com>
2021-04-03 11:28:45 +0700

    Updates

---
+++ precache-manifest.8788ff5fb5641aa352bdce4bdc9146b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bf58e6748ff948ab4891fac8a8f05a",
+    "url": "/fe-blockchain/index.html"
+  },
+  {
+    "revision": "1c21946de0958940b2ed",
+    "url": "/fe-blockchain/static/css/main.b86f500f.chunk.css"
+  },
+  {
+    "revision": "1368bb296bb96236b0dc",
+    "url": "/fe-blockchain/static/js/2.1d72e83a.chunk.js"
+  },
+  {
+    "revision": "e5bfca862d90926c7dd8e4bc0a79f655",
+    "url": "/fe-blockchain/static/js/2.1d72e83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6560f04712c71600717",
+    "url": "/fe-blockchain/static/js/3.99891e53.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/fe-blockchain/static/js/3.99891e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d47f9549c6d8404777e",
+    "url": "/fe-blockchain/static/js/4.e84e5214.chunk.js"
+  },
+  {
+    "revision": "1c21946de0958940b2ed",
+    "url": "/fe-blockchain/static/js/main.292bc147.chunk.js"
+  },
+  {
+    "revision": "535ea6e8573f98285919",
+    "url": "/fe-blockchain/static/js/runtime-main.ca204a63.js"
+  }
+]);
